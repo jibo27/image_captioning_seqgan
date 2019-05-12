7a116063cfb9caa6f01aa417fee47dc219c9694d
orris27 <gsorris27@gmail.com>
2019-05-12 19:22:12 +0800

    add argparse

--- sample.py
+++ sample.py
@@ -11,25 +11,33 @@ from generator import Generator
 from settings import *
 
 
-with open(vocab_path, 'rb') as f:
-    vocab = pickle.load(f)
+def main(args):
+    with open(vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
 
-vocab_size = len(vocab)
-print('vocab_size:', vocab_size)
+    vocab_size = len(vocab)
+    print('vocab_size:', vocab_size)
 
-dataloader = get_loader(image_dir, caption_path, vocab, 
-                        batch_size,
-                        crop_size,
-                        shuffle=True, num_workers=num_workers)
+    dataloader = get_loader(image_dir, caption_path, vocab, 
+                            batch_size,
+                            crop_size,
+                            shuffle=True, num_workers=num_workers)
 
-   
-generator = Generator(attention_dim, embedding_size, lstm_size, vocab_size, load_ad=False)
-generator = generator.to(device)
-generator = generator.eval()
+       
+    generator = Generator(attention_dim, embedding_size, lstm_size, vocab_size, load=args.load)
+    generator = generator.to(device)
+    generator = generator.eval()
 
 
-fullnames = ['data/giraffe.png', 'data/surf.jpg', 'data/bedroom.jpg']
-for fullname in fullnames:
-    print(fullname.split('/')[-1].split('.')[0] + ':')
-    print(generator.generate(fullname, vocab, True))
-    #print(caption = generator.generate(fullname, vocab, False))
+    fullnames = ['data/giraffe.png', 'data/surf.jpg', 'data/bedroom.jpg']
+    for fullname in fullnames:
+        print(fullname.split('/')[-1].split('.')[0] + ':')
+        print(generator.generate(fullname, vocab, True))
+        #print(caption = generator.generate(fullname, vocab, False))
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load', type=str, default='pre', help='which model to load')
+    args = parser.parse_args()
+    main(args)
