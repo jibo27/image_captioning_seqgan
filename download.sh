7f072fc3905367b8af817078f82ca008d80eb693
orris27 <gsorris27@gmail.com>
2019-05-13 12:52:08 +0800

    update

--- download.sh
+++ download.sh
@@ -1,11 +1,29 @@
-mkdir data
-wget http://msvocds.blob.core.windows.net/annotations-1-0-3/captions_train-val2014.zip -P ./data/
-wget http://images.cocodataset.org/zips/train2014.zip -P ./data/
-wget http://images.cocodataset.org/zips/val2014.zip -P ./data/
+cd /content
 
-unzip ./data/captions_train-val2014.zip -d ./data/
+[ -f train2014.zip ] || wget http://images.cocodataset.org/zips/train2014.zip
+[ -f val2014.zip ] || wget http://images.cocodataset.org/zips/val2014.zip
+[ -f annotations_trainval2014.zip ] || wget http://images.cocodataset.org/annotations/annotations_trainval2014.zip
+
+[ -d train2014 ] || unzip -q train2014.zip
+[ -d val2014 ] || unzip -q val2014.zip
+[ -d annotations ] || unzip -q annotations_trainval2014.zip
+
 rm ./data/captions_train-val2014.zip
-unzip ./data/train2014.zip -d ./data/
 rm ./data/train2014.zip 
-unzip ./data/val2014.zip -d ./data/ 
 rm ./data/val2014.zip 
+
+!pip install nltk
+import nltk
+nltk.download('punkt')
+
+git clone https://github.com/pdollar/coco.git 
+cd coco/PythonAPI 
+make 
+python setup.py build
+python setup.py install
+
+# os.chdir("/content/coco/PythonAPI")
+# !make
+# !python setup.py build
+# !python setup.py install
+
