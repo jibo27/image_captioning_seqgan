dd2bdc8a58d5eb0bda3ec521aab9fd2b23f0dd26
orris27 <gsorris27@gmail.com>
2019-05-13 14:12:02 +0800

    add args

--- calculate_bleu.py
+++ calculate_bleu.py
@@ -17,67 +17,77 @@ from generator import Generator
 from settings import *
 
 
-with open(vocab_path, 'rb') as f:
-    vocab = pickle.load(f)
+def main(args):
 
-vocab_size = len(vocab)
-print('vocab_size:', vocab_size)
+    with open(vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
 
-transform = T.Compose([
-    T.ToTensor(),
-    T.Normalize((0.485, 0.456, 0.406),
-                         (0.229, 0.224, 0.225))])
+    vocab_size = len(vocab)
+    print('vocab_size:', vocab_size)
 
-#image_dir = './data/valresized2014/'
-#caption_path = './data/annotations/captions_val2014.json'
-num_workers = 32
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize((0.485, 0.456, 0.406),
+                             (0.229, 0.224, 0.225))])
 
-
-dataloader = get_loader(image_dir, caption_path, vocab, 
-                        batch_size,
-                        crop_size,
-                        shuffle=False, num_workers=num_workers, transform=transform)
+    #image_dir = './data/valresized2014/'
+    #caption_path = './data/annotations/captions_val2014.json'
+    num_workers = 32
 
 
-generator = Generator(attention_dim, embedding_size, lstm_size, vocab_size, load_ad=False)
-generator = generator.to(device)
-generator = generator.eval()
+    dataloader = get_loader(image_dir, caption_path, vocab, 
+                            batch_size,
+                            crop_size,
+                            shuffle=False, num_workers=num_workers, transform=transform)
 
-def translate(indices, vocab):
-    sentences = list()
-    for index in indices:
-        word = vocab.idx2word[int(index)]
-        if word == '<eos>':
+
+    generator = Generator(attention_dim, embedding_size, lstm_size, vocab_size, load=args.load)
+    generator = generator.to(device)
+    generator = generator.eval()
+
+    def translate(indices, vocab):
+        sentences = list()
+        for index in indices:
+            word = vocab.idx2word[int(index)]
+            if word == '<eos>':
+                break
+            sentences.append(word)
+        return ' '.join(sentences)
+
+    scores = list()
+    num_batches = 100
+    print('total length:', len(dataloader), '; we chose %d batches'%(num_batches))
+    for index, (imgs, captions, lengths) in tqdm.tqdm(enumerate(dataloader)):
+        imgs = imgs.to(device)
+
+        features = generator.encoder(imgs)
+        indices_list = generator.sample(features, vocab)
+        for i in range(len(indices_list)):
+            sentence_pred = translate(indices_list[i][1:], vocab)
+            sentence = translate(captions[i][1:], vocab)
+            bleus = list()
+            for j in range(4):
+                weights = [0] * 4
+                weights[j] = 1
+                bleus.append(sentence_bleu([sentence], sentence_pred, weights=weights))
+            scores.append(bleus)
+        if index + 1 == num_batches:
             break
-        sentences.append(word)
-    return ' '.join(sentences)
 
-scores = list()
-num_batches = 100
-print('total length:', len(dataloader), '; we chose %d batches'%(num_batches))
-for index, (imgs, captions, lengths) in tqdm.tqdm(enumerate(dataloader)):
-    imgs = imgs.to(device)
+    scores = np.asarray(scores)
+    print(scores.shape)
 
-    features = generator.encoder(imgs)
-    indices_list = generator.sample(features, vocab)
-    for i in range(len(indices_list)):
-        sentence_pred = translate(indices_list[i][1:], vocab)
-        sentence = translate(captions[i][1:], vocab)
-        bleus = list()
-        for j in range(4):
-            weights = [0] * 4
-            weights[j] = 1
-            bleus.append(sentence_bleu([sentence], sentence_pred, weights=weights))
-        scores.append(bleus)
-    if index + 1 == num_batches:
-        break
+    for i in range(4):
+        print("BLEU{}".format(i + 1))
+        print('mean score:', np.sum(scores[:, i]) / scores.shape[0])
+        print('min score:', np.min(scores[:, i]))
+        print('max score:', np.max(scores[:, i]))
+        print('sum score:', np.sum(scores[:, i]))
 
-scores = np.asarray(scores)
-print(scores.shape)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--load', type=str, default='pre',
+                        help='the generator model to load')
+    args = parser.parse_args()
+    main(args)
 
-for i in range(4):
-    print("BLEU{}".format(i + 1))
-    print('mean score:', np.sum(scores[:, i]) / scores.shape[0])
-    print('min score:', np.min(scores[:, i]))
-    print('max score:', np.max(scores[:, i]))
-    print('sum score:', np.sum(scores[:, i]))
