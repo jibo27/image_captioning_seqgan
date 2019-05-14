a3640246eeda7a06cdb3c1ca13d117f6d4fc283c
orris27 <gsorris27@gmail.com>
2019-05-15 01:31:57 +0800

    debug

--- generator.py
+++ generator.py
@@ -630,7 +630,7 @@ class Generator(torch.nn.Module):
                 ad_generator_path = 'data/ad_generator_params.pkl'
                 torch.save(self.state_dict(), ad_generator_path)
 
-                if (i + 1) % 500 == 0:
+                if (i + 1) % 100 == 0:
                     print('Start saving ad_generator %d'%(i))
                     ad_generator_path = 'data/ad_generator_params_%d.pkl'%(i)
                     torch.save(self.state_dict(), ad_generator_path)
